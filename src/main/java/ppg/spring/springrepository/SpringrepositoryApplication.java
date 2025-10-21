14f1eebadc55dc5fd7f5217da7f8775725732604
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-10-21 16:27:43 +0300

    First commit

---
+++ src/main/java/ppg/spring/springrepository/SpringrepositoryApplication.java
@@ -0,0 +1,13 @@
+package ppg.spring.springrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ppg/spring/springrepository/SpringrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ppg.spring.springrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
