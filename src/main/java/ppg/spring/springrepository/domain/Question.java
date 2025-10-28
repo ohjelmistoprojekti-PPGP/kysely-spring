3cc3e6eec1378a669768bff4a789f1a6dac765a2
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-10-28 16:27:32 +0200

    Kysymys.java -> Question.java

---
+++ src/main/java/ppg/spring/springrepository/domain/Question.java
@@ -0,0 +1,62 @@
+package ppg.spring.springrepository.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    // attribuutit
+    private String questionText;
+
+    @ManyToOne
+    @JoinColumn(name = "questionId")
+    private Survey survey;
+
+    // konstruktorit
+
+    // getit ja setit
+    public Long getquestionId() {
+        return questionId;
+    }
+
+    public Question(Long questionId, String questionText, Survey survey) {
+        this.questionId = questionId;
+        this.questionText = questionText;
+        this.survey = survey;
+    }
+
+    public void setquestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getquestionText() {
+        return questionText;
+    }
+
+    public void setquestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public Survey getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(Survey survey) {
+        this.survey = survey;
+    }
+
+    @Override
+    public String toString() {
+        return "Question [questionId=" + questionId + ", questionText=" + questionText + ", survey=" + survey + "]";
+    }
+
+}
