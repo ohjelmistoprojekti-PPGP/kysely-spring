d0a53c1a01692b55fe3bebd6dc11ce9ac7fc7c0a
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-11-04 16:31:44 +0200

    SurveyRestController

--- src/main/java/ppg/spring/springrepository/web/SurveyRestController.java
+++ src/main/java/ppg/spring/springrepository/web/SurveyRestController.java
@@ -1,15 +1,41 @@
 package ppg.spring.springrepository.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ppg.spring.springrepository.domain.QuestionRepository;
+import ppg.spring.springrepository.domain.Survey;
 import ppg.spring.springrepository.domain.SurveyRepository;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class SurveyRestController {
 
     @Autowired
     private SurveyRepository surveyRepository;
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    // RESTful service to get all surveys
+    @GetMapping("/surveys")
+    public List<Survey> getAllSurveys() {
+	return (List<Survey>) surveyRepository.findAll();
+    }
+    // Yksittäinen kysely
+    @GetMapping("/api/surveys/{id}")
+    public Survey getSurveyById(@PathVariable Long id) {
+    return surveyRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Survey not found: " + id));
+}
+
 
 }
+
+
+
